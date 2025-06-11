7224640d0524fd2a2db7384a7489a8b5d4752970
Nelson Dominguez <ekkolon@proton.me>
2025-06-11 04:46:47 +0200

    test(angular): update types in example tests

--- examples/angular/src/app/app.component.spec.ts
+++ examples/angular/src/app/app.component.spec.ts
@@ -9,18 +9,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome example-angular',
-    );
-  });
-
-  it(`should have as title 'example-angular'`, () => {
+  it(`should have as title 'FireAuth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('example-angular');
+    expect(app.title).toEqual('FireAuth');
   });
 });
